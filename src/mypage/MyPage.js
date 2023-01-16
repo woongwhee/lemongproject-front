6f64dfbedb7baf45f2c43294d07881fe017873ce
SongJiho1998 <106515028+SongJiho1998@users.noreply.github.com>
2023-01-16 21:04:52 +0900

    songjiho_mypage

---
+++ src/mypage/MyPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Component } from "react";
+
+// 마이페이지 css
+// import './MyPage.css';
+
+class MyPage extends Component{
+    render(){
+        return(
+            <div className="outer">
+                <div className="outer_date">
+
+                </div>
+                <div className="outer_">
+
+                </div>
+            </div>
+        )
+    };
+};
+
+export {MyPage};
