9fedbfb94b92fe1bb33e27ad85f7909bbf2c02bc
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2023-01-17 19:15:56 +0900

    프록시 설정완료 백앤드 포트 8081로 실행시 접근가능

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+module.exports = (app) => {
+    app.use(
+        createProxyMiddleware('/api', {
+            target: 'http://localhost:8081',
+            changeOrigin: true,
+        })
+    );
+};
