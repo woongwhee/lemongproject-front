094315dfd41a81e0598f3e34d9a8a45e180e10a6
minseok <mins5714@gmail.com>
2023-01-19 05:05:37 +0900

    1.19

--- src/feedPage/FeedInsert.js
+++ src/feedPage/FeedInsert.js
@@ -1,29 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; // 액시오스
+import axios from 'axios';
+import data from "bootstrap/js/src/dom/data"; // 액시오스
 
 function FeedInsert() {
     const [id, SetId] = useState();
     const [content, SetContent] = useState("");
-    const [picture, SetPicture] = useState("");
+
 
     return (
         <div>
             <p>아이디</p>
             <input onChange={(e) => {SetId(e.target.value);}}/>
             <p>사진</p>
-            <input onChange={(e) => {SetPicture(e.target.value);}}/>
+
             <p>피드 내용</p>
             <input onChange={(e) => {SetContent(e.target.value);}}/>
             <br/>
     <button onClick={ () => {
-        axios.get('api/feed/insert', {
-                params: {
-                    id: id,
-                    picture: picture,
-                    content: content
-                }
-            }).catch(function () {
-                console.log('실패함')
+        axios.post('api/feed/insert', {
+            userNo:id,
+            feedContent:content
+                // params: {
+                //     id: id,
+                //     picture: picture,
+                //     content: content
+                // }
+
+            }).then(function (res){
+                console.log(res.data);
+        })
+            .catch(function () {
+                console.log('실패함' + id, content)
             })
         }}
     >전송</button>
---
+++ src/feedPage/test.js
@@ -0,0 +1,62 @@
+import {useEffect, useState} from "react";
+
+function App() {
+
+    const baseUrl = "http://localhost:8080";
+
+    const [user_username, setUser_username] = useState();
+    const [user_password, setUser_password] = useState();
+
+    useEffect(()=>{
+        getUser();
+    },[]);
+
+    async function getUser(){
+        await axios
+            .get(baseUrl + "/" + {user_username})
+            .then((response) => {
+                console.log(response.data);
+                setUser_username(response.data.userName);
+                setUser_password(response.data.password);
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+    }
+
+    const handleChange_username = (e)=>{
+        e.preventDefault();
+        setUser_username(e.target.value);
+    }
+
+    const handleChange_password = (e)=>{
+        e.preventDefault();
+        setUser_password(e.target.value);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await axios
+            .post(baseUrl + "/" + {user_username}, {
+                userName:user_username,
+                password:user_password,
+            })
+            .then((response) => {
+                console.log(response.data)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <p>username<input type="text" required={true} value={user_username} onChange={handleChange_username}></input></p>
+                <p>password<input type="text" required={true} value={user_password} onChange={handleChange_password}></input></p>
+                <button type="submit">수정</button>
+            </form>
+        </div>
+    )
+}
