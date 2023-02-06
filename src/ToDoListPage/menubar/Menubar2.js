de82d1d517231f0f3f12d12eca2e4cadfd7e8ebb
97minseok <mins5714@gmail.com>
2023-02-06 21:49:34 +0900

    2.06

--- src/ToDoListPage/menubar/Menubar2.js
+++ src/ToDoListPage/menubar/Menubar2.js
@@ -5,6 +5,7 @@ import ChallengeList from "./ChallengeList";
 import TemplateView from "./TemplateView";
 import TodoView3 from "../todolist3/TodoView";
 import { useSelector } from "react-redux";
+import Menubar3 from "./Menubar3";
 
 //const selectTodo = useSelector(state => state.value);
 
@@ -19,7 +20,7 @@ const menuList = {
 class Menubar2 extends Component{
   constructor(props) {
     super();
-    
+
     this.state = {
       menu: 3,
     };
@@ -31,19 +32,21 @@ class Menubar2 extends Component{
 
   render(){
     return(
-      <div className="menubar-box">
-        <div className="menuBar">
-          <ul className="tabs">
-            <li className={`${this.state.menu === 0? 'active': ''}`} onClick={() => this.changeMenu(0)}>TodoList</li>
-            <li className={`${this.state.menu === 1? 'active': ''}`} onClick={() => this.changeMenu(1)}>Feed</li>
-            <li className={`${this.state.menu === 2? 'active': ''}`} onClick={() => this.changeMenu(2)}>Challenge</li>
-            <li className={`${this.state.menu === 3? 'active': ''}`} onClick={() => this.changeMenu(3)}>Template</li>
-          </ul>
-        </div>
-        <div className="contentArea">
-          {menuList[this.state.menu]}
-        </div>
-      </div>
+        <Menubar3></Menubar3>
+      // <div className="menubar-box">
+      //   <div className="menuBar">
+      //     <ul className="tabs">
+      //       <li className={`${this.state.menu === 0? 'active': ''}`}
+      //           onClick={() => this.changeMenu(0)}>TodoList</li>
+      //       <li className={`${this.state.menu === 1? 'active': ''}`} onClick={() => this.changeMenu(1)}>Feed</li>
+      //       <li className={`${this.state.menu === 2? 'active': ''}`} onClick={() => this.changeMenu(2)}>Challenge</li>
+      //       <li className={`${this.state.menu === 3? 'active': ''}`} onClick={() => this.changeMenu(3)}>Template</li>
+      //     </ul>
+      //   </div>
+      //   <div className="contentArea">
+      //     {menuList[this.state.menu]}
+      //   </div>
+      // </div>
     )
   }
 }
--- src/feedPage/FeedMenu.js
+++ src/feedPage/FeedMenu.js
@@ -1,47 +1,40 @@
-import React, {useState} from 'react';
-import FeedInsert from "./FeedInsert";
-import FeedBody from "./FeedBody";
-import Button from "react-bootstrap/Button";
-import Offcanvas from 'react-bootstrap/Offcanvas';
-function FeedMenu(props) {
-
-    const [feedpage,setFeedPage]=useState(true);
-    const pageOn = () => {setFeedPage(true);}
-    const pageOff= () => {setFeedPage(false);}
-
-
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                메뉴
-            </Button>
-
-            <Offcanvas show={show} onHide={handleClose}>
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>메뉴바</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <Button onClick={pageOn}>피드메인</Button> <br/><br/><br/>
-                    <Button onClick={pageOff}>Feed Insert</Button>
-                </Offcanvas.Body>
-            </Offcanvas>
-            {feedpage === true ? <FeedBody/> :  <FeedInsert/>}
-
-
-
-
-
-        </>
-    )
-
-
-
-
-
-}
-
-export default FeedMenu;
+// import React, {useState} from 'react';
+// import FeedInsert from "./FeedInsert";
+// import FeedBody from "./FeedBody";
+// import Button from "react-bootstrap/Button";
+// import Offcanvas from 'react-bootstrap/Offcanvas';
+// import Menubar2 from "../ToDoListPage/menubar/Menubar2";
+// function FeedMenu(props) {
+//
+//     // const [feedpage,setFeedPage]=useState(false);
+//     // const pageOn = () => {setFeedPage(true);}
+//     // const pageOff= () => {setFeedPage(false);}
+//
+//     const [show, setShow] = useState(false);
+//     const handleClose = () => setShow(false);
+//     const handleShow = () => setShow(true);
+//
+//     return (
+//         <>
+//             <Button variant="primary" onClick={handleShow}>
+//                 메뉴
+//             </Button>
+//             <Offcanvas show={show} onHide={handleClose} placement="end">
+//                 <Offcanvas.Header closeButton>
+//                     <Offcanvas.Title>메뉴바</Offcanvas.Title>
+//                 </Offcanvas.Header>
+//                 <Offcanvas.Body>
+//                     <Button>Feed Insert</Button>
+//                 </Offcanvas.Body>
+//             </Offcanvas>
+//             {/*{feedpage === true ? <FeedBody/> :  <FeedInsert/>}*/}
+//         </>
+//     )
+//
+//
+//
+//
+//
+// }
+//
+// export default FeedMenu;
