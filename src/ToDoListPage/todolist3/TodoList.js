b4ad0721fe8165cf35f200904ac7df4b6014c643
pinesolhan <carpe.sol.sun@gmail.com>
2023-02-10 19:50:35 +0900

    드래그앤드롭 최최종

--- src/ToDoListPage/todolist3/TodoList.js
+++ src/ToDoListPage/todolist3/TodoList.js
@@ -14,17 +14,12 @@ const TodoListBlock = styled.div`
 
 function TodoList({todoList, setTodoList, chList, onDel, onToggle, onUpdate, onDelay}) {
 
-  //const chTodos = chList[0].todoList;
-  //console.log(chTodos);
-  let arr = [];
-  
-  
-  const onDragStart = (res) => {
+//  const onDragStart = (res) => {
     //const originArr = todoList.map(todo => todo.todoNo);
-    arr.push(res.draggableId); //선택한 투두 no
-    //console.log(todoList);
-    console.log("시작한 곳"+res.draggableId);
-  }
+    //arr.push(res.draggableId); //선택한 투두 no
+    console.log(todoList);
+    //console.log(res);
+//  }
   
   const onDragEnd = (res) => {
     if (!res.destination) return;
@@ -32,36 +27,53 @@ function TodoList({todoList, setTodoList, chList, onDel, onToggle, onUpdate, onD
     const dndTodoList = [...todoList];
     const [reorderedItem] = dndTodoList.splice(res.source.index, 1);
     dndTodoList.splice(res.destination.index, 0, reorderedItem);
-
-    //const changeArr = dndTodoList.map(todo => todo.todoNo);
-
-    console.log("시작 인덱스"+res.source.index);
-    console.log("도착 인덱스"+res.destination.index);
-    console.log(res);
-    console.log(res.source);
-    console.log(res.destination);
+    setTodoList(dndTodoList);
     console.log(dndTodoList);
 
-    const originValue = res.source.index;
-    const changeValue = res.destination.index;
+    //const changeArr = todoList.map(todo => todo.todoNo);
 
-    dndTodoList.map((todo,res)=>{
-      if(todo.todoNo=== res.destination){
-        todo.value = changeValue;
-      }
-    })
-    
-    setTodoList(dndTodoList);
-    console.log("수정후"+dndTodoList);
+    // console.log("시작 인덱스"+res.source.index);
+    // console.log("도착 인덱스"+res.destination.index);
+    //console.log(res);
+    //console.log(res.source);
+    //console.log(res.destination);
+    //console.log(dndTodoList);
 
-    axios.get('api/todo/dndTodo', {
-      params : { dndTodoNo : res.draggableId }
-    }).then(function(){
-      console.log("최종"+arr);
+    // const originValue = res.source.index;
+    // const changeValue = res.destination.index;
+
+    // todoList.map((todo,res)=>{
+    //   if(todo.todoNo=== res.destination){
+    //     todo.value = changeValue;
+    //   }
+    // })
+
+    // axios.get('api/todo/dndTodos', {
+    //   params : { dndTodoNo : res.draggableId,
+    //               value : res.destination.index }
+    // }).then(function(){
+    //   console.log("최종"+arr);
+    //   console.log("dnd 완료");
+    // }).catch(function(){
+    //   console.log("dnd 실패")
+    // })
+
+    axios.post('api/todo/dndTodo3', ({
+       dndTodoList : dndTodoList 
+    })).then(function(){
       console.log("dnd 완료");
     }).catch(function(){
       console.log("dnd 실패")
     })
+
+    // axios.post('api/todo/dndTodo', ({
+    //  dndTodo : changeArr.join(',')
+    // })).then(function(){
+    //   console.log("최종"+arr);
+    //   console.log("dnd 완료");
+    // }).catch(function(){
+    //   console.log("dnd 실패")
+    // })
 
     
   }
@@ -69,12 +81,12 @@ function TodoList({todoList, setTodoList, chList, onDel, onToggle, onUpdate, onD
   return ( 
     <>
     <p>Daily Todo-List</p>
-    <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd} >
+    <DragDropContext onDragEnd={onDragEnd} >
       <Droppable droppableId="drop-area">
         {provided => (
           <TodoListBlock {...provided.droppableProps} ref={provided.innerRef}>
             {todoList && todoList.map((todo, index) =>(
-              <Draggable draggableId={String(todo.todoNo)} index={todo.value} key={index} >
+              <Draggable draggableId={String(todo.todoNo)} index={index} key={index} >
                 {provided => (
                   <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                   <TodoItem
