5c1145382c3185bb2115c4dd3f8e02c2351c106a
miseon-park <72717093+miseon-park@users.noreply.github.com>
2023-02-19 15:44:29 +0900

    카카오 배포 주소 추가

--- src/member/KakaoLoginData.js
+++ src/member/KakaoLoginData.js
@@ -1,7 +1,11 @@
 export const CLIENT_ID = '6c1bbd8efca92b427aff16845e3336d1';
+// 로컬 redirect
 export const REDIRECT_URI = 'http://localhost:3000/kakao';
 export const LOGOUT_REDIRECT_URI = 'http://localhost:3000';
-// export const BACK_REDIRECT_URI = 'http://localhost:8081/api/p/kakaoLogin';
+
+// 배포 redirct
+// export const REDIRECT_URI = 'http://52.194.242.239/kakao';
+// export const LOGOUT_REDIRECT_URI = 'http://52.194.242.239';
 // 인가 코드 + 토큰을 받아오는 uri -> 프론트로 받아오기
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
