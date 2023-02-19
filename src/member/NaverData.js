bbc04f56f5a314726a276c8a95d074170305558b
miseon-park <72717093+miseon-park@users.noreply.github.com>
2023-02-19 16:00:28 +0900

    네이버 배포 주소 추가

--- src/member/NaverData.js
+++ src/member/NaverData.js
@@ -2,4 +2,4 @@ export const Client_Id = 'gby5MZqE_2ShDpfOnFIS';
 export const Client_Secret = 'OyOjXUomy0';
 
 export const Naver_CallBack_URL = 'http://localhost:3000/naver';
-// export const Naver_CallBack_URL = 'http://localhost:3000/naverWhite';
+// export const Naver_CallBack_URL = 'http://52.194.242.239/naver';
