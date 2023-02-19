18d838e7064034d18f669f0f69ab6a3a11c0d8ff
pinesolhan <carpe.sol.sun@gmail.com>
2023-02-19 18:42:44 +0900

    챌린지 모집 페이지 css 중

--- src/challenge/component/ChallengeDetail.js
+++ src/challenge/component/ChallengeDetail.js
@@ -3,6 +3,8 @@ import apiHoc from "../../util/apiHoc";
 import {Button} from "reactstrap";
 import {cancelMulti, joinMulti} from "../challengeApi";
 import {useLoginState} from "../../member/LoginContext";
+import '../style/ChallengeList.css';
+import { BsArrowLeftCircle} from 'react-icons/bs'
 
 const ChallengeDetail = ({result}) => {
     const {challengeNo, challengeTitle, challengeInfo, startDate, LocalDate, endDate,readyUsers,todoPreview} = result;
@@ -17,9 +19,32 @@ const ChallengeDetail = ({result}) => {
         alert(res);
     }
     
+    const countUser = readyUsers.length;
+    
+    const chStartDate = (startDate[0]).toString()+"년 "+(startDate[1].toString())+"월 "+(startDate[2]).toString()+"일"; 
+    const chEndDate = (endDate[0]).toString()+"년 "+(endDate[1].toString())+"월 "+(endDate[2]).toString()+"일";     
+    const chStartDate2 = (endDate[0]).toString()+"년 "+(endDate[1].toString())+"월 "+(endDate[2]+1).toString()+"일"; 
+
     return (
-        <div>
-            {isReady?<Button onClick={join}>흐에</Button>:<Button onClick={cancel}>포기할래요</Button>}
+        <div className='chDetail'>
+            <div className='dtBtn'>
+                <div className='btnBack'>
+                    <BsArrowLeftCircle />
+                </div>
+            </div>
+            <div className='btnss'>
+                {isReady?<Button onClick={join}>흐에</Button>:<Button onClick={cancel}>포기할래요</Button>}
+            </div>
+            <div className='dtTitle'>{challengeTitle}</div>
+            <div className='dtDate'>모집일 : {chStartDate}~{chEndDate}</div>
+            <div className='dtCountUser'>참여인원 : {countUser}</div>
+            <div className='dtStart'>시작일 : {chStartDate2}</div>
+            <div className='dtInfo'>챌린지 소개 : {challengeInfo}</div>
+
+            <div className='dtTodo'>{todoPreview.todoList.map(todo=>todo.todoContent)}</div>
+            
+            <div className='dtCountUser2'>참여 인원 ( {countUser} )</div>
+            <div className='dtUser'>{readyUsers.map(user=>user.nickName)}</div>
         </div>
     );
 };
