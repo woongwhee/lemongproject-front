38f1c2550dde55420a832b6534d108b463c4fc29
miseon-park <72717093+miseon-park@users.noreply.github.com>
2023-02-19 14:55:36 +0900

    첫 화면에 카피라이터 추가

--- src/member/MainFront.js
+++ src/member/MainFront.js
@@ -8,14 +8,16 @@ function MainFront() {
     
     return(
         <>
-            
-            
-             <TitleInfo />
+            <TitleInfo />
 
-            
             <Login />
+
+            <div className="copyRightArea">
+                <p>LEMONG PROJECT</p>
+                <p>(c) 2023. 연필뽀개기 Inc. all rights reserved 진웅휘 | 박미선 | 송지호 | 장민석 | 한솔</p>
+            </div>
             
-             <div className='loginBackColor'></div>
+            <div className='loginBackColor'></div>
 
         </>
     )
